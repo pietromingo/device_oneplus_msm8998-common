5eee3f50c0fb3ad7d6db9f12c00da5b7b6de7746
NurKeinNeid <mralexman3000@gmail.com>
2019-12-22 17:39:52 +0100

    Revert "[DNM] msm8998-common: Introduce 'SafailNet'"
    This reverts commit 2e9ed76d7b562fac9a1b315687faaf20a970f439.
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- prebuilts/Android.mk
+++ prebuilts/Android.mk
@@ -4,14 +4,6 @@ include $(CLEAR_VARS)
 # files that live under device/qcom/common/rootdir/etc/
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.safailnet.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.safailnet.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := GoogleCamera
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := GoogleCamera.apk
