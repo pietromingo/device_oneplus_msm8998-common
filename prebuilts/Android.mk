0f87b143905465af6fac3c8ecc058ec6cbe6b6d8
NurKeinNeid <mralexman3000@gmail.com>
2019-12-30 22:43:59 +0100

    msm8998-common: Add  OnePlus Gallery from Android Q DP3
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- prebuilts/Android.mk
+++ prebuilts/Android.mk
@@ -1,7 +1,4 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-
-# files that live under device/qcom/common/rootdir/etc/
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := GoogleCamera
@@ -12,4 +9,15 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
 LOCAL_OVERRIDES_PACKAGES := Camera2
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := OnePlusGallery
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := OnePlusGallery.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
+LOCAL_OVERRIDES_PACKAGES := Gallery2
 include $(BUILD_PREBUILT)
