e1f2210229df6f611593754b8f24cae785d30bcf
NurKeinNeid <mralexman3000@gmail.com>
2020-01-21 21:11:26 +0100

    Revert "msm8998-common: Add zipaligned Arnova-Urnyx05-Gcam7.apk"
    This reverts commit 5cae325378b5f21ecbf0f69670c07537a6f32600.
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- common.mk
+++ common.mk
@@ -143,8 +143,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    libshim_camera \
-    GoogleCamera
+    libshim_camera
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
