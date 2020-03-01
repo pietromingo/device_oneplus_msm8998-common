89e494bd2eb173d1bf741801ecb0cca9eaa4d272
NurKeinNeid <mralexman3000@gmail.com>
2020-03-01 19:30:08 +0100

    msm8998-common: BoardConfig: Switch to baremetal GCC
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -81,8 +81,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
 TARGET_KERNEL_CONFIG := derp_defconfig
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := $(shell sh -c "find prebuilts/clang/host/linux-x86/ -type f -name clang -printf \"%T@ %Tc %p\n\" | sort -n | tail -n1 | sed 's/.*clang-\(.*\)\/bin.*/\1/'")
+TARGET_KERNEL_NEW_GCC_COMPILE := true
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
